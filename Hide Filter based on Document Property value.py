8137d1ecfe2dadd59e8221da39f54cf50b3ab031
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 23:23:36 +0530

    Add files via upload

---
+++ Hide Filter based on Document Property value.py
@@ -0,0 +1,15 @@
+from Spotfire.Dxp.Application.Filters import *
+from Spotfire.Dxp.Application import PanelTypeIdentifiers
+ 
+filterPanel = Document.ActivePageReference.FilterPanel
+if Document.Properties["justchecking"] == "anupam":
+	for tableGroup in filterPanel.TableGroups:
+		for fh in tableGroup.FilterHandles:
+			if fh.Visible:
+				if fh.FilterReference.Name =="Region":
+					fh.Visible = False
+else:
+	for tableGroup in filterPanel.TableGroups:
+		for fh in tableGroup.FilterHandles:
+				if fh.FilterReference.Name =="Region":
+					fh.Visible = True

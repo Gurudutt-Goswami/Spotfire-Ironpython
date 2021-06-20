7eda03e25cbdb9cd3431891c4c34651e597046c7
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 21:16:09 +0530

    Add files via upload

---
+++ Get distinct values of columns.py
@@ -0,0 +1,35 @@
+#get unique values from column (7.0)
+from Spotfire.Dxp.Data import DataValueCursor
+from Spotfire.Dxp.Data import IndexSet
+
+tableName = 'Sample Data'
+columnName = "Region"
+columnName1 = "Item"
+dt = Document.Data.Tables[tableName]
+cursor = DataValueCursor.Create[str](dt.Columns[columnName])
+distinctRows = dt.GetDistinctRows(None,cursor) #none param to grab them by the all
+#distinctRows = dt.GetDistinctRows(IndexSet(dt.RowCount,True),cursor) #...or an indexset with all true to get them all.
+
+#get uniques
+vals = []
+distinctRows.Reset()
+while distinctRows.MoveNext():vals.append(cursor.CurrentValue)
+
+#show results
+
+print vals
+
+dt = Document.Data.Tables[tableName]
+cursor = DataValueCursor.Create[str](dt.Columns[columnName1])
+distinctRows = dt.GetDistinctRows(None,cursor) #none param to grab them by the all
+#distinctRows = dt.GetDistinctRows(IndexSet(dt.RowCount,True),cursor) #...or an indexset with all true to get them all.
+
+#get uniques
+vals = []
+distinctRows.Reset()
+while distinctRows.MoveNext():vals.append(cursor.CurrentValue)
+
+#show results
+
+
+print vals

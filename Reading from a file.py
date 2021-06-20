dd31a142e6549d8c24e8ee2d08a2aef350bd50fb
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 21:39:15 +0530

    Add files via upload

---
+++ Reading from a file.py
@@ -0,0 +1,9 @@
+from System.IO import Path, StreamWriter
+from Spotfire.Dxp.Application.Visuals import CrossTablePlot
+
+targetFileName = "C:\Users\hp\Desktop\guru\ test.txt"
+aLine=open(r'C:\Users\hp\Desktop\guru\test.txt').read()
+ 
+#Debug
+print targetFileName
+print aLine

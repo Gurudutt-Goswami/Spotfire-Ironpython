434068536137cf024becddb9bbd41734560df959
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 16:49:12 +0530

    Update Clear All Script.py

--- Clear All Script.py
+++ Clear All Script.py
@@ -1,3 +1,9 @@
+'''
+Note: 
+If you clear all filters and selecting things one by one and no graph is changing then you have to find proper hierarchy for the columns like which column is dependent on other column and then you have to organize it as well.
+To organize a filter, right click on filters > organize filters > check only those filters which are independent and leave the rest. A good practice is to align filter in a hierarchal way according to their dependencies.
+'''
+
 from Spotfire.Dxp.Application.Visuals import VisualContent
 from Spotfire.Dxp.Application import Filters as filters
 from Spotfire.Dxp.Application.Filters import FilterTypeIdentifiers

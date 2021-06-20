0dfe08859257dae08430bb7e4e8086e7241cd988
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-21 00:36:21 +0530

    Add files via upload

---
+++ Select all for a Hierarchy Column.py
@@ -0,0 +1,9 @@
+import Spotfire.Dxp.Application.Filters as filters
+import Spotfire.Dxp.Application.Filters.ListBoxFilter
+from Spotfire.Dxp.Application.Filters import FilterTypeIdentifiers
+from Spotfire.Dxp.Data import DataPropertyClass
+from System import String
+myPanel = Document.ActivePageReference.FilterPanel
+myFilter= myPanel.TableGroups[0].GetFilter("Hierarchy")
+lbFilter = myFilter.FilterReference.As[filters.CheckBoxHierarchyFilter]()
+lbFilter.Reset()

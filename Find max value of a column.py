5e4b294ccc32a164a995469b3bf82eaa809e315b
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 23:17:36 +0530

    Update Find max value of a column.py

--- Find max value of a column.py
+++ Find max value of a column.py
@@ -1,3 +1,5 @@
+#Add test_data as a script parameter & define your table 
+#In place of "Units" put your column name for which you want to find max 
 activeFilteringSelection = Document.ActiveFilteringSelectionReference.GetSelection(test_data).AsIndexSet()
 column = test_data.Columns["Units"]
 max = column.RowValues.GetMaxValue(activeFilteringSelection).ValidValue

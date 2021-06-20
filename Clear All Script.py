ecb3efe31feb7299b761837b8349113244b25d63
Gurudutt-Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-06-20 16:33:32 +0530

    Add files via upload

---
+++ Clear All Script.py
@@ -0,0 +1,30 @@
+from Spotfire.Dxp.Application.Visuals import VisualContent
+from Spotfire.Dxp.Application import Filters as filters
+from Spotfire.Dxp.Application.Filters import FilterTypeIdentifiers
+
+# Get the filter panel on the current active page
+filterPanel = Document.ActivePageReference.FilterPanel
+# Perform filtering action
+
+for tableGroup in filterPanel.TableGroups:
+	for fh in tableGroup.FilterHandles:
+	   
+		if fh.Visible:
+			print fh.FilterReference.Name
+			if fh.FilterReference.TypeId.Name == "Spotfire.CheckBoxFilter" or 	fh.FilterReference.TypeId.Name == "Spotfire.CheckBoxHierarchyFilter":
+		
+				
+					#print fh.FilterReference.TypeId.Name
+					if (fh.FilterReference.TypeId == FilterTypeIdentifiers.CheckBoxFilter):
+						cbFilter = fh.FilterReference.As[filters.CheckBoxFilter]()
+				
+					#In the following line you have write column names in place of JobID,dept,gender,location
+						if fh.FilterReference.Name =="Region" or fh.FilterReference.Name =="Item":
+					#print fh.FilterReference.Name 
+								for checkedValues in cbFilter.Values:
+										#print fh.FilterReference.TypeId.Name
+									
+										cbFilter.Uncheck(checkedValues) 
+										cbFilter.IncludeEmpty = False 
+					if (fh.FilterReference.TypeId.Name == "Spotfire.CheckBoxHierarchyFilter"):
+						fh.FilterReference.As[filters.CheckBoxHierarchyFilter]().UncheckAllNodes()

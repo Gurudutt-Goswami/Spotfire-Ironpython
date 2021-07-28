64d91d21b7ddc1edab4fbae52b8bd4f3ed93a234
Gurudutt Goswami <86184439+Gurudutt-Goswami@users.noreply.github.com>
2021-07-28 19:35:39 +0530

    Update Export Table from active visualisation.py

--- Export Table from active visualisation.py
+++ Export Table from active visualisation.py
@@ -6,7 +6,8 @@ from System.IO import File
 writer = Document.Data.CreateDataWriter(DataWriterTypeIdentifiers.ExcelXlsDataWriter)
 table = Document.ActiveDataTableReference #OR pass the DataTable as parameter
 filtered = Document.ActiveFilteringSelectionReference.GetSelection(table).AsIndexSet() #OR pass the filter
-stream = File.OpenWrite("C:/Users/hp/Desktop/guru/nice.csv")
+stream = File.OpenWrite("C:/Users/hp/Desktop/guru/nice.csv") #For excel write .xls not xlsx & for text write .txt 
+# In case you want to have all three format at once , you can have three stream writing consecutively 
 names = []
 for col in table.Columns: 
     names.append(col.Name)
